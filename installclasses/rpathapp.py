292ac3bf84c9ed10d3b0fa649829f2d5050706c8
Michael Tharp <mtharp@rpath.com>
2011-05-03 16:53:34 -0400

    Add a new install class "rpathapp" [rpath]

---
+++ installclasses/rpathapp.py
@@ -0,0 +1,34 @@
+#
+# Copyright (c) 2011 rPath, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import gettext
+_ = lambda x: gettext.ldgettext("anaconda", x)
+
+import tarinstall
+from rhel import InstallClass as RHELInstallClass
+
+
+class InstallClass(RHELInstallClass):
+    id = 'rpath'
+    name = _('rPath Appliance Base')
+    description = _('default install class for rPath based appliances')
+
+    sortPriority = 25000
+    hidden = 1
+
+    def getBackend(self):
+        return tarinstall.TarBackend
